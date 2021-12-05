4b36c290802c7b33907f6f3547b1a43eac942e6d
hanekawa933 <iqbalkorompiz@gmail.com>
2021-12-05 23:58:13 +0700

    ganti

--- LeNET_fme/datasets/emnist.py
+++ LeNET_fme/datasets/emnist.py
@@ -15,14 +15,14 @@ def get_emnist(train):
                                         std=params.dataset_std)])
 
     # dataset and data loader
-    emnist_dataset = datasets.EMNIST(root=data_root,split='mnist',
+    emnist_dataset = datasets.EMNIST(root=params.data_root,split='mnist',
                                 train=train,
                                 transform=pre_process,
                                 download=True)
 
     emnist_data_loader = torch.utils.data.DataLoader(
         dataset=emnist_dataset,
-        batch_size=batch_size,
+        batch_size=params.batch_size,
         shuffle=True)
 
     return emnist_data_loader
