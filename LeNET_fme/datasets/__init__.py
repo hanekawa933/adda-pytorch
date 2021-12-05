64797e30562230cb30945dda86dbbfd0e0c075cf
hanekawa933 <iqbalkorompiz@gmail.com>
2021-12-05 22:28:25 +0700

    First Push

---
+++ LeNET_fme/datasets/__init__.py
@@ -0,0 +1,4 @@
+from .emnist import get_emnist
+from .fmnist import get_fmnist
+
+__all__ = (get_emnist, get_fmnist)
---
+++ LeNET_fme/datasets/emnist.py
@@ -0,0 +1,26 @@
+"""Dataset setting and data loader for MNIST."""
+
+
+import torch
+from torchvision import datasets, transforms
+
+def get_emnist(train):
+    """Get EMNIST dataset loader."""
+    # image pre-processing
+    pre_process = transforms.Compose([transforms.ToTensor(),
+                                      transforms.Normalize(
+                                          mean=dataset_mean,
+                                          std=dataset_std)])
+
+    # dataset and data loader
+    emnist_dataset = datasets.EMNIST(root=data_root,split='mnist',
+                                   train=train,
+                                   transform=pre_process,
+                                   download=True)
+
+    emnist_data_loader = torch.utils.data.DataLoader(
+        dataset=emnist_dataset,
+        batch_size=batch_size,
+        shuffle=True)
+
+    return emnist_data_loader
